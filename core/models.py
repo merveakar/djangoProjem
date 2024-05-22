472908e9be1ea26e3714ca25c58e3d142d2746f2
Merve Akar <merveakar2506@gmail.com>
2024-05-23 01:48:29 +0300

    create layout

--- core/models.py
+++ core/models.py
@@ -6,26 +6,46 @@ class GeneralSetting(models.Model):
         max_length=254,
         blank=True,
         default='',
+        verbose_name='Name',
+        help_text='This is variable of general setting',
     )
     description = models.CharField(
         max_length=254,
         blank=True,
         default='',
+        verbose_name='Description'
+
     )
     parameter = models.CharField(
         max_length=254,
         blank=True,
         default='',
+        verbose_name='Parameter'
+
     )
     status = models.BooleanField(  # Yeni alan eklendi
         default=True,
+        verbose_name='Status'
+
     )
     updated_date = models.DateTimeField(
         auto_now=True,
         blank=True,
+        verbose_name='Updated Date'
+
     )
     created_date = models.DateTimeField(
         auto_now_add=True,
         blank=True,
+        verbose_name='Created Date'
+
     )
 
+
+    def __str__(self):
+        return f'GeneralSetting: {self.name}'
+
+    class Meta:
+        verbose_name = 'General Setting'
+        verbose_name_plural = 'General Settings'
+        ordering = ('name',)
