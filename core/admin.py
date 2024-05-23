f194088e2061b6dd5fd9dfd0ba76992c491c6849
Merve Akar <merveakar2506@gmail.com>
2024-05-23 13:14:19 +0300

    new commit

--- core/admin.py
+++ core/admin.py
@@ -13,3 +13,11 @@ class GeneralSettingAdmin(admin.ModelAdmin):
         model = GeneralSetting
 
 
+@admin.register(ImageSetting)
+class ImageSettingAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description', 'file', 'updated_date', 'created_date']
+    search_fields = ['name', 'description', 'file']
+    list_editable = ['description', 'file']
+
+    class Meta:
+        model = ImageSetting
