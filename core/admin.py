36561e66786273df3b5341b73d0ee6f420a9f679
Merve Akar <merveakar2506@gmail.com>
2024-05-23 12:33:59 +0300

    new commit

--- core/admin.py
+++ core/admin.py
@@ -2,12 +2,11 @@ from django.contrib import admin
 from .models import *
 
 
-
 @admin.register(GeneralSetting)
 class GeneralSettingAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'description', 'parameter']
     search_fields = ['name', 'description', 'parameter']
-    list_editable = ['description','parameter']
+    list_editable = ['description', 'parameter']
+
     class Meta:
         model = GeneralSetting
-
